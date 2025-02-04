41265cd96f1a80595f8178f07de1991e582f1925
ErAashishRanjan <er.aashishranjan@gmail.com>
2025-02-04 14:00:09 +0530

    checking initial capacity

---
+++ ArrayList2.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class ArrayList2 {
+    public static void main(String[] args) {
+        ArrayList arrayList= new ArrayList(11);
+        System.out.println("size is "+arrayList.size());
+        arrayList.add(10);
+        arrayList.add(10);
+        arrayList.add(10);
+        arrayList.add(10);
+        arrayList.add(10);
+        arrayList.add(10);
+        arrayList.add(10);
+        arrayList.add(10);
+        arrayList.add(10);
+        arrayList.add(10);
+        arrayList.add(10);
+        System.out.println(arrayList);
+
+    }
+}
